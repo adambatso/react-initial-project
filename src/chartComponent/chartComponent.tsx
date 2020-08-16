c5373fd0215492a9559ffd4dfab7a742a4bb7234
Adamtso <adam.ben.aharon@theculturetrip.com>
2020-08-16 14:34:35 +0300

    Add react d3 components

--- src/chartComponent/chartComponent.tsx
+++ src/chartComponent/chartComponent.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import * as React from 'react';
 import { ChartTitle } from './chartComponent.style';
+const ReactD3 = require('react-d3-components');
 
 export const ChartComponent = () => {
-    return <ChartTitle>
+    const [y, setY] = React.useState(10)
+    const BarChart = ReactD3.BarChart;
+    const data = [{
+        label: 'somethingA',
+        values: [{x: 'SomethingA', y: y}, {x: 'SomethingB', y: 4}, {x: 'SomethingC', y: 3}]
+    }];
+    console.log('%c y', 'font-size:20px;color:lime;', y);    
+    return <><ChartTitle>
         This is my first Chart
     </ChartTitle>
+    <button onClick={() => {setY(Math.ceil(Math.random() * 20))}}>clickOnMeToChange</button>
+    <BarChart
+        data={data}
+        width={400}
+        height={400}
+        margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
+    </>
 }
